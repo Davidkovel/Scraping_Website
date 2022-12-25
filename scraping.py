d9c351be55af378dc18aec4ffc34542d8ecb72cb
Davidkovel <113856260+Davidkovel@users.noreply.github.com>
2022-12-25 22:02:00 +0000

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,40 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import time
+
 
 CSV = "laptops.csv"
-url = "https://www.olx.ua/d/uk/elektronika/noutbuki-i-aksesuary/"
+url = "https://answear.ua/k/vona/odyag/bluzki"
+host = 'https://answear.ua'
 headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 OPR/92.0.0.0"
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0"
     }
-# bot check
+
 def get_data(url, params=''):
     req = requests.get(url, headers=headers, params=params)
     return req
+
 def get_content(html):
     soup = BeautifulSoup(html, "html.parser")
-    items = soup.find_all("div", class_="css-qfzx1y")
+    items = soup.find_all('div', class_='m-4 l-3 Products__productsFullWide__mix1Y xs-6')
+    # images = soup.find_all("img")
     laptops = []
 
     for item in items:
-        laptops.append({
-            'title': item.find('h6', class_="css-1pvd0aj-Text eu5v0x0").get_text(strip=True),
-            'price': item.find('p', class_="css-1q7gvpp-Text eu5v0x0").get_text(strip=True),
-            'city': item.find('p', class_="css-p6wsjo-Text eu5v0x0").get_text(strip=True),
-            })
-
+        ttl = item.find('div', class_ = "ProductItem__productCardName__DCKIH").get_text(strip=True)
+        prc = item.find('div', class_='Price__salePrice__FCFFF ProductItem__priceSale__XP3ik')
+        if prc is not None:
+            prc = prc.get_text(strip=True)
+            laptops.append({
+                'title': ttl,
+                'price': prc,
+                'imagee': item.find('picture', class_='Image__cardImage__xvgs1').find('img').get('src'),
+                'get_link': host + item.find('a').get('href')
+             })
     return laptops
-    # global laptop
-    # for laptop in laptops:
-    #     print(f"{laptop['title']} : {laptop['price']} : {laptop['city']} - {laptop['link_href']}")
+
 
 def save_doc(items, path):
-    with open(path, 'w', newline='') as file:
+    with open(path, 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file, delimiter=';')
-        writer.writerow(["Название", "Цена", "Город"])
+        writer.writerow(["Название", "Цена", "Картинка", "Сылка на продукт"])
         for item in items:
-            writer.writerow([item["title"], item["price"], item["city"]])
+            writer.writerow([item["title"], item["price"], item['imagee'], item['get_link']])
 
 
 def parsing_page():
@@ -46,8 +53,9 @@ def parsing_page():
         for page in range(1, pagenation+1):
             print(f"Enter Page {page}")
             html = get_data(url, params={'page': page})
+            time.sleep(2)
             laptops.extend(get_content(html.text))
-            save_doc(laptops, CSV)
+        save_doc(laptops, CSV)
         print("Parsing finished")
     else:
         print("Error")
